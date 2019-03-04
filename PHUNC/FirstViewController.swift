04bd490d1744aab22392333a5e074082cad45ef1
Mihir Alve <mihir@Meatball.local>
2019-03-04 17:15:55 -0500

    added bars back in

--- PHUNC/FirstViewController.swift
+++ PHUNC/FirstViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import WebKit
 import SDWebImage
-//import ImageSlideshow
+import ImageSlideshow
 
 class FirstViewController: UIViewController, UIWebViewDelegate {
 
@@ -21,20 +21,21 @@ class FirstViewController: UIViewController, UIWebViewDelegate {
     @IBOutlet weak var container: UIView!
     
     @IBOutlet weak var logoImg: UIImageView!
-    
-//    @IBOutlet weak var sponsors: ImageSlideshow!
+
+    @IBOutlet weak var sponsors: ImageSlideshow!
     
     @IBOutlet var Parent: UIView!
     @IBOutlet weak var webViewWidth: NSLayoutConstraint!
 //    let sponsorSource = [SDWebImageSource(urlString: "https://phunc.psiada.org/wp-content/uploads/2017/12/CGS-logo.png")!, SDWebImageSource(urlString: "https://phunc.psiada.org/wp-content/uploads/2018/10/O_lSUAaA_400x400.jpg")!, SDWebImageSource(urlString: "https://phunc.psiada.org/wp-content/uploads/2018/10/c6IsO5zb_400x400.jpg")!, SDWebImageSource(urlString: "https://phunc.psiada.org/wp-content/uploads/2018/10/psu-agsciences-logo.png")!, SDWebImageSource(urlString: "https://phunc.psiada.org/wp-content/uploads/2018/10/German_Header_Responsive_stacked-2.png")!, SDWebImageSource(urlString: "https://phunc.psiada.org/wp-content/uploads/2018/10/logo_sia_retina.png")!, SDWebImageSource(urlString: "https://phunc.psiada.org/wp-content/uploads/2018/10/Rock-Ethics-Wordmark-1.png")!, SDWebImageSource(urlString: "https://phunc.psiada.org/wp-content/uploads/2018/10/PSUIST.png")!, SDWebImageSource(urlString: "https://phunc.psiada.org/wp-content/uploads/2018/10/psu_bwo_rgb_2c_3x_1.png")!,]
 //
-    //let sponsorSource = [ImageSource(image: #imageLiteral(resourceName: "pla_logo")), ImageSource(image: #imageLiteral(resourceName: "seven_sage")), ImageSource(image: #imageLiteral(resourceName: "seton_hall")), ImageSource(image: #imageLiteral(resourceName: "SIA-logo")), ImageSource(image: #imageLiteral(resourceName: "Schreyer-Logo"))]
+    let sponsorSource = [ImageSource(image: #imageLiteral(resourceName: "pla_logo")), ImageSource(image: #imageLiteral(resourceName: "seven_sage")), ImageSource(image: #imageLiteral(resourceName: "seton_hall")), ImageSource(image: #imageLiteral(resourceName: "SIA-logo")), ImageSource(image: #imageLiteral(resourceName: "Schreyer-Logo"))]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         
+//        sponsors.setImageInputs(sponsorSource)
       
 //        sponsors.setImageInputs(sponsorSource)
 //        sponsors.slideshowInterval = 5
